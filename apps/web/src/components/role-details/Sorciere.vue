4e40f859b86b4f387241048cfa83c00857d3497b
Marc Prats <marc.prats11@gmail.com>
2026-09-13 22:56:23 +0200

    fix: enhance text formatting for Sorciere role details and improve header styles

--- apps/web/src/components/role-details/Sorciere.vue
+++ apps/web/src/components/role-details/Sorciere.vue
@@ -6,14 +6,16 @@ defineProps<{ powerTitle: string; infoTitle: string; currentRoleId: string }>()
 
 <template>
     <RoleDetailSection :title="powerTitle" icon="⚡">
-        <p class="app-role-text">Si vous mourez la nuit, vous choisissez une personne et découvrez
+        <p class="app-role-text">Si vous <strong>mourez la nuit</strong>, vous choisissez une
+            personne et <strong>découvrez</strong>
             son identité.</p>
     </RoleDetailSection>
     <RoleDetailSection :title="infoTitle" icon="💡">
         <p class="app-role-text">Votre pouvoir se déclenche à votre mort.<br>N'hésitez pas à vous
             faire
             passer pour un personnage en possession d'informations afin d'attirer la morsure d'un
-            Loup Garou.<br>Si vous vous faites éliminer par le village, votre pouvoir ne se
+            Loup Garou.<br>Si vous vous faites <strong>éliminer par le village</strong>, votre
+            pouvoir ne se
             déclenchera
             pas.</p>
     </RoleDetailSection>
